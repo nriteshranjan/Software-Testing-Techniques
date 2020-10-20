a2fb04833971c900246e5891bf2cabe8a13d5880
rrite <nriteshranjan@gmail.com>
2020-10-20 20:36:22 +0530

    Created test file for inherited class

---
+++ Employee/src/test/java/Employee/ManagerTest.java
@@ -0,0 +1,38 @@
+package Employee;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPayRate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPayRate() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pay() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSalaried() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSalaried() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testPay() {
+    }
+}
---
+++ Employee/src/test/java/Employee/SupervisorTest.java
@@ -0,0 +1,28 @@
+package Employee;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SupervisorTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getPayRate() {
+    }
+
+    @Test
+    void setPayRate() {
+    }
+
+    @Test
+    void pay() {
+    }
+}
